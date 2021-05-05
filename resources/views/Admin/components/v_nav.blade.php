56a872485e587bd6b02ac6fe8f941699421d15a7
Zudha Pratama <zudhapratama123@gmail.com>
2021-05-05 13:19:04 +0700

    Edit link active navbar

--- resources/views/Admin/components/v_nav.blade.php
+++ resources/views/Admin/components/v_nav.blade.php
@@ -13,43 +13,43 @@
           <!-- Nav items -->
           <ul class="navbar-nav">
             <li class="nav-item">
-              <a class="nav-link" href="/">
+              <a class="nav-link{{request()->is('/') ? ' active' : ''}}" href="/">
                 <i class="ni ni-tv-2 text-primary"></i>
                 <span class="nav-link-text">Home</span>
               </a>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="/dosen">
+              <a class="nav-link{{request()->is('dosen') ? ' active' : ''}}" href="/dosen">
                 <i class="ni ni-planet text-orange"></i>
                 <span class="nav-link-text">Profile Dosen</span>
               </a>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="/mahasiswa">
+              <a class="nav-link{{request()->is('mahasiswa') ? ' active' : ''}}" href="/mahasiswa">
                 <i class="ni ni-pin-3 text-primary"></i>
                 <span class="nav-link-text">Karya mahasiswa</span>
               </a>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="/judulpa">
+              <a class="nav-link{{request()->is('judulpa') ? ' active' : ''}}" href="/judulpa">
                 <i class="ni ni-bullet-list-67 text-default"></i>
                 <span class="nav-link-text">Judul PA</span>
               </a>
             </li>
             <li class="nav-item">
-              <a class="nav-link " href="/user">
+              <a class="nav-link{{request()->is('user') ? ' active' : ''}}" href="/user">
                 <i class="ni ni-single-02 text-yellow"></i>
                 <span class="nav-link-text">User</span>
               </a>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="/industri">
+              <a class="nav-link{{request()->is('/industri') ? ' active' : ''}}" href="/industri">
                 <i class="ni ni-building text-info"></i>
                 <span class="nav-link-text">Industri</span>
               </a>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="login.html">
+              <a class="nav-link{{request()->is('/login') ? ' active' : ''}}" href="login.html">
                 <i class="ni ni-key-25 text-info"></i>
                 <span class="nav-link-text">Logout</span>
               </a>
