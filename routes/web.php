f3c0595c4d1b391b7ea6841458dc756095108eed
Arief Graifhan <ariefgraifhan@gmail.com>
2021-05-07 14:02:48 +0700

    route main

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+// custom route
+Route::view('/main', 'main');
