c3313edd694f611cdc328882212a63b9724a7524
Arief Graifhan <ariefgraifhan@gmail.com>
2021-05-11 10:38:41 +0700

    after scaffolding to react

--- app/Models/DosenRpl.php
+++ app/Models/DosenRpl.php
@@ -4,8 +4,35 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
 
 class DosenRpl extends Model
 {
     use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+        'jenis_kelamin',
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    protected $casts = [
+        'thn_bergabung' => 'datetime',
+    ];
 }
