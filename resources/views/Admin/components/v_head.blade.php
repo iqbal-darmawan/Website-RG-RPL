14fa572461857b3865a0412559aa3ff76384689b
Arief Graifhan <ariefgraifhan@gmail.com>
2021-05-11 11:59:14 +0700

    changing FA to cdn

--- resources/views/Admin/components/v_head.blade.php
+++ resources/views/Admin/components/v_head.blade.php
@@ -10,7 +10,7 @@
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700">
     <!-- Icons -->
     <link rel="stylesheet" href="{{asset('template/')}}/assets/vendor/nucleo/css/nucleo.css" type="text/css">
-    <link rel="stylesheet" href="{{asset('template/')}}/assets/vendor/@fortawesome/fontawesome-free/css/all.min.css" type="text/css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" type="text/css">
     <!-- Argon CSS -->
     <link rel="stylesheet" href="{{asset('template/')}}/assets/css/argon.css?v=1.2.0" type="text/css">
   </head>
